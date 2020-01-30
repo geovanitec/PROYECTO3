c9b953ebd53d5cc418c716dc59ff9b09810c4233
geovani Mendoza <geovanifermen@gmail.com>
2020-01-30 11:33:41 -0600

    laboratorio 2

---
+++ LABORATORIO_2_PROGRA_3.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio_2_progra_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Geovani
+ */
+public class LABORATORIO_2_PROGRA_3 {
+
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer =new Scanner(System.in);
+       String nombre=" ", apellido=" ",puesto=" ";
+        float salario=0,sueldoextra=0,sordinario=0,hextra=0,comision=0,tdevengado=0,igss=0;
+        float isr=0,dlab=0,anti=0,boni=0,tdescuentos=0,tliquido=0,pdiario=0,otros=0,descuento=0;
+        
+        
+        //datos c[]=new datos[2];
+        
+        
+        float matriz[][] = new float[10][16];
+        
+        
+        for ( float c =0; c < matriz.length; c++){
+            
+            for (float f=0; f< matriz.length;f++){
+                
+             
+                        
+                
+            }
+        }
+    }
+    
+}
